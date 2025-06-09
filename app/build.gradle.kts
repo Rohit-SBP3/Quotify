5255b300fdb8d8dd411ce6adf735b3b44ba31cb0
Rohit Singh <rohitrajput95547@gmail.com>
2025-06-09 08:48:30 +0530

    Espresso Test(next and Share Buttons)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,7 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
+    testImplementation("junit:junit:4.12")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test:core:1.6.1")
     androidTestImplementation("androidx.test:runner:1.6.1")
@@ -73,4 +74,5 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+    testImplementation(kotlin("test"))
 }
--- app/src/androidTest/java/com/example/quotify/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/quotify/MainActivityTest.kt
@@ -1,14 +1,24 @@
 package com.example.quotify
 
+
+import android.content.Intent
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
-import androidx.test.ext.junit.runners.AndroidJUnit4
-import org.junit.Rule
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasAction
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import kotlinx.coroutines.withContext
+import org.hamcrest.CoreMatchers.allOf
 import org.junit.Test
-import org.junit.runner.RunWith
+import org.junit.Assert.*
+import org.junit.Rule
 
-@RunWith(AndroidJUnit4::class)
+
 class MainActivityTest {
 
     @get:Rule
@@ -16,9 +26,24 @@ class MainActivityTest {
 
     @Test
     fun testNextButton_expected_correctQuote() {
+        /*onView(withId(R.id.btnNext)).perform(click())
+        onView(withId(R.id.btnNext)).perform(click())
+        onView(withId(R.id.btnNext)).perform(typeText(""))
 
-        //onView(withId(R.id.btnNext))
+        onView(withId(R.id.btnNext)).check(matches(withText("Dream big and dare to fail.")))*/
+    }
+
+    @Test
+    fun testShareButton_expected_intentChooser() {
+
+        /*Intents.init()
+
+        val expected = allOf(hasAction(Intent.ACTION_SEND))
+        onView(withId(R.id.fab)).perform(click())
+        intended(expected)
+        Intents.release()*/
 
     }
 
+  
 }
